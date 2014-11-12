546edaa5168ac7c3bc84d21a4123526a8ad2c0a4
jollen <jollen@moko365.com>
2014-11-12 23:39:26 +0800

    use port 4800

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.port = process.env.PORT || 3000;
+exports.port = process.env.PORT || 4800;
 exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/innoboard'
 };
